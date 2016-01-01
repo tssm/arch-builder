b62176599e80d60f63dd36d2d62ce0bc58aedff4
Tae Sandoval <tssm@users.noreply.github.com>
2015-12-31 21:33:52 -0300

    Initial commit

---
+++ box-up.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+readonly NATURAL_REGEX='^[0-9]+$'
+readonly OS_TYPE="ArchLinux_64"
+
+readonly DEFAULT_MACHINE_NAME="Test"
+readonly DEFAULT_DRIVE_SIZE=3000
+readonly DEFAULT_MEMORY_SIZE=1024
+
+echo Machine name, default ${DEFAULT_MACHINE_NAME}:
+read MACHINE_NAME
+if [ "${MACHINE_NAME}" = "" ]; then
+	MACHINE_NAME=${DEFAULT_MACHINE_NAME}
+fi
+
+while :
+do
+	echo "Machine memory (in mebibytes), default ${DEFAULT_MEMORY_SIZE}":
+	read MACHINE_MEMORY
+	if [[ "${MACHINE_MEMORY}" =~ $NATURAL_REGEX ]]; then
+		break
+	elif [ "${MACHINE_MEMORY}" = "" ]; then
+		MACHINE_MEMORY=$DEFAULT_MEMORY_SIZE
+		break
+	fi
+done
+
+while :
+do
+	echo "Drive size (in megabytes), default $DEFAULT_DRIVE_SIZE":
+	read DRIVE_SIZE
+	if [[ "${DRIVE_SIZE}" =~ $NATURAL_REGEX ]]; then
+		break
+	elif [ "${DRIVE_SIZE}" = "" ]; then
+		DRIVE_SIZE=$DEFAULT_DRIVE_SIZE
+		break
+	fi
+done
+
+readonly DRIVE_PATH="$HOME/VirtualBox VMs/${MACHINE_NAME}/${MACHINE_NAME}"
+
+VBoxManage createvm --name "${MACHINE_NAME}" --ostype "${OS_TYPE}" --register
+VBoxManage createmedium disk --filename "${DRIVE_PATH}" --size "${DRIVE_SIZE}" --variant Fixed
+VBoxManage storagectl "${MACHINE_NAME}" --name SATA --add sata --controller IntelAHCI
+VBoxManage storageattach "${MACHINE_NAME}" --storagectl SATA --port 0 --type hdd --medium "${DRIVE_PATH}.vdi"
+VBoxManage modifyvm "${MACHINE_NAME}" --firmware efi
+VBoxManage modifyvm "${MACHINE_NAME}" --memory "${MACHINE_MEMORY}"
+VBoxManage modifyvm "${MACHINE_NAME}" --boot1 disk
+VBoxManage modifyvm "${MACHINE_NAME}" --boot2 none
+VBoxManage modifyvm "${MACHINE_NAME}" --boot3 none
+VBoxManage modifyvm "${MACHINE_NAME}" --boot4 none
+# TODO: Add a bridged network
---
+++ set-up.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# Hostname
+while :
+do
+	echo "Enter hostname:"
+	read HOSTNAME
+	if [[ "${HOSTNAME}" != "" ]]; then
+		echo "${HOSTNAME}" > /etc/hostname
+		break
+	fi
+done
+
+# Locales
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+
+# Time
+ln -s /usr/share/zoneinfo/America/Santiago /etc/localtime
+
+echo "[Time]" > /etc/systemd/timesyncd.conf
+echo "NTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org" >> /etc/systemd/timesyncd.conf
+echo "FallbackNTP=0.pool.ntp.org 1.pool.ntp.org 0.fr.pool.ntp.org" >> /etc/systemd/timesyncd.conf
+
+systemctl enable systemd-timesyncd
+
+# Sudoers
+echo "Defaults editor=/usr/bin/nvim" > /etc/sudoers
+echo "root ALL=(ALL) ALL" >> /etc/sudoers
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Users
+passwd
+
+while :
+do
+	echo "Enter user name:"
+	read USERNAME
+	if [[ "${USERNAME}" != "" ]]; then
+		useradd -m -G wheel -s /bin/bash ${USERNAME}
+		passwd ${USERNAME}
+		break
+	fi
+done
+
+echo "auth required pam_wheel.so use_uid" >> /etc/pam.d/su
+echo "auth required pam_wheel.so use_uid" >> /etc/pam.d/su-l
+echo "hvc0" >> /etc/securetty
+
+# Firewall
+systemctl enable nftables
+
+# SSH
+echo "AllowGroups wheel" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "#PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+systemctl enable sshd.socket
+
+# Systemd default target
+systemctl set-default multi-user.target
+
+# Kernel
+echo "blacklist i2c_piix4" > /etc/modprobe.d/i2c_piix4.conf
+# VirtualBox does not provide smbus
+
+# Network
+echo "[Match]" > /etc/systemd/network/10-default.network
+echo "[Network]" >> /etc/systemd/network/10-default.network
+echo "DHCP=yes" >> /etc/systemd/network/10-default.network
+
+systemctl enable systemd-networkd
+
+# Boot loader
+bootctl --path=/boot install
+
+echo "default arch" > /boot/loader/loader.conf
+echo "timeout 0" >> /boot/loader/loader.conf
+echo "editor 0" >> /boot/loader/loader.conf
+
+echo "title Arch Linux" > /boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux-lts" >> /boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux-lts.img" >> /boot/loader/entries/arch.conf
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda2) rw init=/usr/lib/systemd/systemd" >> /boot/loader/entries/arch.conf
+
+echo "Done! Now quit and don't forget to umount -R /mnt before reboot"
