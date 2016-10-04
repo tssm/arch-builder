75d85ce9d9f08ddd99315fe9b657afdbee6279e2
Tae Sandoval <tssm@users.noreply.github.com>
2016-10-04 13:21:02 -0300

    Comment out statements related to existing databases

--- etc/set-up-postgresql.sql
+++ etc/set-up-postgresql.sql
@@ -9,5 +9,6 @@ create role backups login;
 alter default privileges in schema public grant select on sequences to backups;
 alter default privileges in schema public grant select on tables to backups;
 
-grant select on all tables in schema public to backups;
-grant select, usage on all sequences in schema public to backups;
+--- Only needed if already exist at least one database:
+--- grant select on all tables in schema public to backups;
+--- grant select, usage on all sequences in schema public to backups;
