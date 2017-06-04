3b477a820f989ba28f2d16b68dcc32119e1e2671
Tae Sandoval <tssm@users.noreply.github.com>
2017-06-04 13:42:55 -0400

    Fix production time step

--- bin/time.sh
+++ bin/time.sh
@@ -10,7 +10,11 @@ do
 	echo "Enter time sub-zone:"
 	read TIME_SUB_ZONE
 	if [[ -n "${TIME_ZONE}" && -n "${TIME_SUB_ZONE}" ]]; then
-		ln -s /usr/share/zoneinfo/"${TIME_ZONE}"/"${TIME_SUB_ZONE}" /etc/localtime
+		if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
+			ln -sf /usr/share/zoneinfo/"${TIME_ZONE}"/"${TIME_SUB_ZONE}" /etc/localtime
+		else
+			timedatectl set-timezone "${TIME_ZONE}"/"${TIME_SUB_ZONE}"
+		fi
 		break
 	fi
 done
